3abb71a9e30b7d31acbc002115b2fc7768f3bec2
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-05-22 07:12:43 +0900

    feat: header add

--- components/original/header.tsx
+++ components/original/header.tsx
@@ -1,5 +1,22 @@
+'use client'
+import ThemeButton from '@/components/original/theme-button'
+import { site } from '@/data/data'
+import Link from 'next/link'
+
 const Header = () => {
-  return <header>header</header>
+  return (
+    <header className="mt-5 mb-4">
+      <div className="container max-w-4xl flex justify-between">
+        <div className="flex items-center">
+          <div>
+            <Link href="/">{site.title}</Link>
+          </div>
+        </div>
+
+        <ThemeButton />
+      </div>
+    </header>
+  )
 }
 
 export default Header
