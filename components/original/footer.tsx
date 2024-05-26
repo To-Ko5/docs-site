ac02b42470915b63e22c6f9b12d1f6630ae64bbf
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-05-26 09:05:38 +0900

    feat: footer add

--- components/original/footer.tsx
+++ components/original/footer.tsx
@@ -1,5 +1,14 @@
+import { site } from '@/data/data'
+import Link from 'next/link'
+
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className="p-3 border-t">
+      <p className="text-right text-sm text-muted-foreground mr-4">
+        <Link href="/">{site.title}</Link>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
