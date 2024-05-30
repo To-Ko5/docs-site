0ca9308fcc50f8223f6f03522496f7e1d710cdac
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-05-31 08:54:16 +0900

    feat: blog component add

---
+++ components/original/blog.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+
+const Blog = () => {
+  return (
+    <Card>
+      <CardHeader>Blog</CardHeader>
+
+      <CardContent>
+        <p>test1</p>
+
+        <p>test2</p>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Blog
