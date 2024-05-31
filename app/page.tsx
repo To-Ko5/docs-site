8cd65e733ab45012bbd49dc3763524404766be35
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-06-01 05:22:42 +0900

    feat: component arrangement

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Blog from '@/components/original/blog'
+
 export default function Home() {
   return (
     <main className="container max-w-4xl">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-        laudantium eos earum temporibus cum ut dolorum, repellendus beatae
-        suscipit facilis ab ipsam. Odit excepturi ipsam provident! Aliquid fugit
-        minus hic.
-      </p>
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+      </div>
     </main>
   )
 }
