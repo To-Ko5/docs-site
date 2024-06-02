5f0fd32bef010e7a8e6e517b6d2e5d0a9691735c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-06-03 08:59:26 +0900

    feat: date component add

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Blog from '@/components/original/blog'
+import CurrentDate from '@/components/original/current-date'
 
 export default function Home() {
   return (
     <main className="container max-w-4xl">
+      <div className="mb-8">
+        <CurrentDate />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Blog />
         <Blog />
