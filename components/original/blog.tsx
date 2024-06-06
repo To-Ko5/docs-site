822f4659843995957cdaf254e072f6914779eb81
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-06-07 08:31:42 +0900

    feat: blog layout add

--- components/original/blog.tsx
+++ components/original/blog.tsx
@@ -7,16 +7,24 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
 
 const Blog = () => {
   return (
     <Card>
-      <CardHeader>Blog</CardHeader>
+      <CardHeader>
+        <h2 className="font-semibold">Blog</h2>
+      </CardHeader>
 
       <CardContent>
-        <p>test1</p>
-
-        <p>test2</p>
+        <div className="flex items-center justify-between">
+          <div className="w-full text-center">test1</div>
+          <Separator
+            orientation="vertical"
+            className="h-8 mx-3 bg-muted-foreground/30"
+          />
+          <div className="w-full text-center">test2</div>
+        </div>
       </CardContent>
     </Card>
   )
